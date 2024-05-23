1e65975860d3fb3c7efb5118733467c44e3161ff
beatriz saito <beatriz.s.gobira@gmail.com>
2024-05-22 22:03:22 -0400

    ver a url do bagui amarelo, e ver a var de ambiente. arruma css

---
+++ aulas/vue/carrosel/carrossel/carrossel/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="css">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+}
+</style>
+
+<script setup>
+import apiClient from './plugins/axios'
+
+apiClient.get('funcionario')
+  .then((response) => {
+    console.log(response.data)
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+</script>
---
+++ aulas/vue/carrosel/carrossel/carrossel/src/components/Menu.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="menu-principal">
+        <div class="logo">
+            <img class="img-menu" src="../assets/hub.png" alt="logo">
+        </div>
+        <div class="redes-sociais">
+            <img class="social" src="../assets/whats-logo.png" alt="logo whatsapp">
+            <img class="social" src="../assets/insta-logo.png" alt="logo insta">
+            <img class="social" src="../assets/git-logo.png" alt="logo github">
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+    export default { 
+        name:'MenuComponent'
+    }
+</script>
+
+<style lang="css"> 
+.menu-principal{
+    position: relative;
+    display: flex;
+    width: 100%;
+    height: 10vh;
+    background-color: rgba(98, 150, 209, 0.296);
+
+    .logo{
+        width: 60%;
+
+        .img-menu{
+            margin-top: 10px;
+            padding: 0 20px;
+        }
+        
+    }
+
+    .redes-sociais{
+        width: 40%;
+    }
+}
+
+</style>
