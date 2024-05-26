5f84c8c1af55802a1634d0568139ba129e0aa07b
beatriz saito <beatriz.s.gobira@gmail.com>
2024-05-26 01:30:03 -0400

    menu configurado, ok!

--- aulas/vue/carrossel/src/components/Menu.vue
+++ aulas/vue/carrossel/src/components/Menu.vue
@@ -1,13 +1,13 @@
 <template>
-    <nav>
-        <div class="logo">
-            <img class="img-menu" src="../assets/hub.png" alt="logo">
+    <nav class="menu-top"> <!-- responsivo, distancia entre os itens, joga uma % de ocupação -->
+        <div>
+            <img class="px-5" src="../assets/hub.png" alt="logo">
         </div>
-        <div class="p-10 hover:bg-purple rounded">
-            <ul>
-                <li ><a target="_blank" href="https://www.whatsapp.com"><img class="social" src="../assets/whats-logo.png" alt="logo whatsapp"></a></li>
-                <li ><a target="_blank" href="https://www.instagram.com"><img class="social" src="../assets/insta-logo.png" alt="logo instagram"></a></li>
-                <li ><a target="_blank" href="https://github.com"><img class="social" src="../assets/git-logo.png" alt="logo github"></a></li>
+        <div class="">
+            <ul class="logo-sociais-tot">
+                <li class="list" ><a target="_blank" href="https://www.whatsapp.com"><img class="h-[12vh]" src="../assets/whats-logo.png" alt="logo whatsapp"></a></li>
+                <li class="list" ><a target="_blank" href="https://www.instagram.com"><img class="h-[12vh]" src="../assets/insta-logo.png" alt="logo instagram"></a></li>
+                <li class="list" ><a target="_blank" href="https://github.com"><img class="h-[12vh]" src="../assets/git-logo.png" alt="logo github"></a></li>
             </ul>
         </div>
     </nav>
@@ -21,6 +21,19 @@
 </script>
 
 <style lang="css"> 
+.menu-top{
+    @apply w-[92%] flex justify-between items-center;
+
+}
+
+.logo-sociais-tot{
+    @apply flex items-center gap-[1vw]
+
+}
+.list{
+    @apply hover:bg-[#5050eb] rounded-full transition-all duration-300
+}
+
 
 /*
 overflow -> hidden tudo que ultrpassar ele, sera ocultado/escondido 
